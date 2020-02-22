fa7a5c3012cc24d8f592dca0f54f0dbd74374b21
snakagam <snakagam@yahoo-corp.jp>
2020-02-23 00:32:38 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import App from "./App";
+import UIkit from "uikit";
+import "uikit/dist/css/uikit.css";
+import "uikit/dist/css/uikit.min.css";
 
 Vue.config.productionTip = false;
 
